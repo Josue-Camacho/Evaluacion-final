ffe3d311822185738ae3c7bde8cca6df517a0c44
Josue-Camacho <josuecamacho582@gmail.com>
2025-12-19 17:09:16 -0400

    feat: add changes in IAuthService

--- Services/IAuthService.cs
+++ Services/IAuthService.cs
@@ -1,6 +1,8 @@
-﻿using ProyectoFinalTecWeb.Entities.Dtos.Auth;
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoFinalTecWeb.Entities.Dtos.Auth;
 using ProyectoFinalTecWeb.Entities.Dtos.DriverDto;
 using ProyectoFinalTecWeb.Entities.Dtos.PassengerDto;
+using ProyectoFinalTecWeb.Services;
 
 namespace ProyectoFinalTecWeb.Services
 {
@@ -11,5 +13,13 @@ namespace ProyectoFinalTecWeb.Services
         Task<string> RegisterPassengerAsync(RegisterPassengerDto dto);
         Task<(bool ok, LoginResponseDto? response)> RefreshAsync(RefreshRequestDto dto);
 
+        Task<string?> ForgotPasswordAsync(string email);
+        Task<bool> ResetPasswordAsync(ResetPasswordDto dto);
     }
 }
+
+
+
+
+
+
